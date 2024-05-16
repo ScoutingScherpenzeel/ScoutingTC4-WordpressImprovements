01314c123244cfb6ac295e052b265d4d0737996e
Melvin Snijders <mail@melvinsnijders.nl>
2024-05-16 23:36:53 +0200

    feat: Converted to child theme

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+add_action('wp_enqueue_scripts', 'scouting_enqueue_styles');
+
+function scouting_enqueue_styles()
+{
+	// Load the CSS improvements
+	wp_enqueue_style(
+		'scouting-improved-style',
+		get_stylesheet_uri()
+	);
+
+	// Load all CSS of the default Scouting template
+	wp_enqueue_style(
+		'thema-style',
+		get_parent_theme_file_uri('style.css'),
+		array('bootstrap-style', 'font-awesome-5-style', 'poppins-google-fonts-style', 'roboto-google-fonts-style')
+	);
+}
