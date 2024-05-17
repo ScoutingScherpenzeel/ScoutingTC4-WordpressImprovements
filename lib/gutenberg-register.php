bf8a610bd548be9a9791ddd0263c31d50c633073
Melvin Snijders <25155102+MelvinSnijders@users.noreply.github.com>
2024-05-17 09:49:06 +0200

    feat: Support for editing post count in latest news

--- lib/gutenberg-register.php
+++ lib/gutenberg-register.php
@@ -34,7 +34,7 @@ function scoutingimproved_acf_json_load_point($paths)
 add_action( 'after_setup_theme', 'scoutingimproved_modify_acf_json_paths' );
 function scoutingimproved_modify_acf_json_paths() {
     // Save ACF JSON in the child theme directory
-    add_filter( 'acf/settings/save_json', 'scoutingimproved_acf_json_save_point' );
+    add_filter( 'acf/settings/save_json', 'childtheme_acf_json_save_point' );
 
     // Add the child theme's acf-json path to the load paths
     add_filter( 'acf/settings/load_json', 'scoutingimproved_acf_json_load_point' );
@@ -68,9 +68,13 @@ function scoutingimproved_acf_latest_news_block_render_callback($block, $content
 	$context['block']      = $block;
 	$context['fields']     = get_fields();
 	$context['is_preview'] = $is_preview;
+
+    // Get the custom field 'post_count', with a default value of 3 if not set
+    $post_count = !empty($context['fields']['post_count']) ? $context['fields']['post_count'] : 3;
 	$context['featured'] = new Timber\PostQuery(array(
 		'post_type' => 'post',
-		'posts_per_page' => 3,
+		'posts_per_page' => $post_count,
 	));
+
 	Timber::render('gutenberg-blocks/gb-latest-news-items.twig', $context);
 }
