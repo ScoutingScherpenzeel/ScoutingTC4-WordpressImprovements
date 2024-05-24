5eb01bfba2d68da07dcb31e283ee1427d65df3e8
Melvin Snijders <25155102+MelvinSnijders@users.noreply.github.com>
2024-05-24 22:44:26 +0200

    fix: Synax error due to missing semicolon

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ function scouting_enqueue_styles()
 	wp_enqueue_style(
 		'scouting-butterfly-style',
 		get_template_directory_uri() . '/assets/css/butterfly.css'
-	)
+	);
 
 	// Load all CSS of the default Scouting template
 	wp_enqueue_style(
